9ae45a04edd7cfcaa1fef6a4191cf9a20acfff06
ariandodev <ariandomtuwe@gmail.com>
2025-07-27 17:42:06 +0700

    Implemented Hot Reload Module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()} or http://localhost:${process.env.PORT || 3000}`);
+  console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
+  console.log(`Port: ${process.env.PORT || 3000}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+    console.log('Hot module replacement enabled');
+  }
 }
 bootstrap();
