b73999aadc04c271dd4d8bc3ba13a2a777d8ba53
ariandodev <ariandomtuwe@gmail.com>
2025-07-27 19:19:54 +0700

    Imported typeorm in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Import TypeORM module with MySQL configuration
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nestjs_rbac',
+      entities: [],
+      // Be sure to set synchronize to false in production.
+      // Synchronize is used to automatically create database tables based on our entities.
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
