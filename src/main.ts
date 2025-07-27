6f72772bbc0b3a4b88d222157d4da243cfa513ab
ariandodev <ariandomtuwe@gmail.com>
2025-07-27 17:45:17 +0700

    Added comments to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,18 @@ import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
+  // Create the NestJS application
   const app = await NestFactory.create(AppModule);
+  
+  // Start the application
   await app.listen(process.env.PORT ?? 3000);
+
+  // Log the application URL and environment details for development only
   console.log(`Application is running on: ${await app.getUrl()} or http://localhost:${process.env.PORT || 3000}`);
   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
   console.log(`Port: ${process.env.PORT || 3000}`);
 
+  // Enable hot module replacement 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
